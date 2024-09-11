b56d07f9a220f1b3ad8d16cfbbc7a24634dbff15
qhseraphim <143456538+qhseraphim@users.noreply.github.com>
2024-09-11 13:02:43 +0200

    started working on intro exercises: function and lists

--- fp_template/src/main/scala/intro/Lists.scala
+++ fp_template/src/main/scala/intro/Lists.scala
@@ -62,5 +62,8 @@ object Lists {
       * Read the Scaladoc on the List class (https://www.scala-lang.org/api/2.12.3/scala/collection/immutable/List.html)
       * It contains some useful functions for this exercise. For this question library functions are allowed.
       */
-    def customAverage(xs: List[Int], x: Int, n: Int): Int = ???
+    def customAverage(xs: List[Int], x: Int, n: Int): Int = {
+        xs.filter((i)=>(i>x)).take(n).sum/n
+
+    }
 }
